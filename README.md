18d0665ebd0d32ac64488d714f59ea28ed622157
Dowsley <joao.dowsley850@gmail.com>
2023-03-19 18:44:20 -0300

    Improve movement animations + add README

---
+++ README.md
@@ -0,0 +1,16 @@
+# The Dark Series
+## Premise 1
+50 years ago, Humanity got into a war with an invading intelligent species of highly advanced  beings. War broke out, and we were no match to their technology, almost indistinguishable from magic.
+
+Our only chance was to use their own weapons against them, augmenting ourselves, and turning the whole planet into a military effort, sacrificing our freedom to save us from annihilation, and win the war.
+
+They say the war lasted one week. Truth is, all we know from this time, is that no one ever won. Humanity as a whole was torn apart, with decentralized pockets of survivors left with a bleeding, war-torn home, filled with weapons from the past and the remnants of the invading forces, spread out through the planet. left crippled, and forced to survive within the planet.
+
+Still, there are some who believe the war is still going. Some believe there's deeper secret.
+
+There, a hero will rise.
+
+## Premise 2
+Earth was ravaged by a war that ended a week and no one ever won. It's effects though, would last forever.  
+Most perished, the rich left it, although no one knows how and to where.  
+And those who stayed, slowly learn how to live in a new home, barren and filled with weapons from the past.
--- src/entities/Player.gd
+++ src/entities/Player.gd
@@ -14,45 +14,67 @@ enum SPRITE_DIRS {
 @export_range(0.0 , 1.0) var acceleration := 0.25
 @export_range(0.0, 50) var jump_and_fall_transition_threshold := 5
 
-
-
 @onready var anim_player := $AnimatedSprite2D
 
-var curr_sprite_dir = SPRITE_DIRS.RIGHT
+# Movement and animation members
+var h_motion_input := 0.0
+var jump_just_pressed_input := false
+var jump_just_released_input := false
+var is_running := false 
+var curr_sprite_dir := SPRITE_DIRS.RIGHT
 
-func _process(delta):
-	velocity.y += gravity * delta
-	var dir := Input.get_axis("walk_left", "walk_right")
-	var is_running := Input.is_action_pressed("run")
-	
-	if dir != 0:
-		anim_player.play("run_fast" if is_running else "walk")
-		var new_dir := SPRITE_DIRS.LEFT if dir < 0 else SPRITE_DIRS.RIGHT
-		if new_dir != curr_sprite_dir:
-			set_sprite_dir(new_dir)
-		velocity.x = lerp(velocity.x, dir * (run_speed if is_running else walk_speed), acceleration)
+func set_sprite_dir(new_dir: int):
+	curr_sprite_dir = new_dir
+
+	if new_dir == SPRITE_DIRS.RIGHT:
+		position.x += SPRITE_DIST_TO_CENTER
+		anim_player.flip_h = false
 	else:
-		anim_player.play("idle")
+		position.x -= SPRITE_DIST_TO_CENTER
+		anim_player.flip_h = true
+
+func loop_controls():
+	h_motion_input = Input.get_axis("walk_left", "walk_right")
+	jump_just_pressed_input = Input.is_action_just_pressed("jump")
+	jump_just_released_input = Input.is_action_just_released("jump")
+	is_running = Input.is_action_pressed("run")
+
+func loop_movement(delta: float):
+	velocity.y += gravity * delta
+	h_motion_input = Input.get_axis("walk_left", "walk_right")
+	is_running = Input.is_action_pressed("run")
+	
+	if h_motion_input != 0:
+		var new_dir := SPRITE_DIRS.LEFT if h_motion_input < 0 else SPRITE_DIRS.RIGHT
+		velocity.x = lerp(velocity.x, h_motion_input * (run_speed if is_running else walk_speed), acceleration)
+	else:
 		velocity.x = lerp(velocity.x, 0.0, friction)
 
 	move_and_slide()
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
 
-	# if velocity.y != 0:
-	# 	if velocity.y > jump_and_fall_transition_threshold:
-	# 		anim_player.play("fall")
-	# 	elif velocity.y < -jump_and_fall_transition_threshold:\
-	# 		anim_player.play("jump")
-	# 	else:
-	# 		anim_player.play("jump_trans")
-	
 
-func set_sprite_dir(new_dir: int):
-	curr_sprite_dir = new_dir
-	if new_dir == SPRITE_DIRS.RIGHT:
-		position.x += SPRITE_DIST_TO_CENTER
-		anim_player.flip_h = false
-	elif new_dir == SPRITE_DIRS.LEFT:
-		position.x -= SPRITE_DIST_TO_CENTER
-		anim_player.flip_h = true
+func loop_animation():
+	var dir := SPRITE_DIRS.LEFT if h_motion_input < 0.0 else SPRITE_DIRS.RIGHT
+	if dir != curr_sprite_dir:
+		set_sprite_dir(dir)
+
+	if is_on_floor():
+		if h_motion_input != 0.0:
+			anim_player.play("run_fast" if is_running else "walk")
+		else:
+			anim_player.play("idle")
+	else:
+		if velocity.y != 0.0:
+			if velocity.y > jump_and_fall_transition_threshold:
+				anim_player.play("fall")
+			elif velocity.y < -jump_and_fall_transition_threshold:
+				anim_player.play("jump")
+			else:
+				anim_player.play("jump_trans")
+
+func _process(delta: float):
+	loop_controls()
+	loop_movement(delta)
+	loop_animation()
